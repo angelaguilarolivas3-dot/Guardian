4e28d1cbb6285593f56f249aaa77971451ae2e8d
angel <angelaguilarolivas3@gmail.com>
2025-12-01 13:59:44 -0700

    Update spam_language.py

--- detectors/spam_language.py
+++ detectors/spam_language.py
@@ -1,1 +1,20 @@
+from db import add_signal
 
+SUSPICIOUS_PHRASES = [
+    "dm me",
+    "free nitro",
+    "trust me",
+    "verify here",
+    "onlyfans",
+    "pornhuh"
+]
+
+def check_message(message):
+    content = message.content.lower()
+    for phrase in SUSPICIOUS_PHRASES:
+        if phrase in content:
+            add_signal(
+                message.guild.id,
+                message.author.id,
+                "suspicious_phrase"
+            )
