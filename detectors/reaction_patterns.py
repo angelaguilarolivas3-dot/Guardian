596ffa369944cf08563a627ff56b9fd7c78c338c
angel <angelaguilarolivas3@gmail.com>
2025-12-01 13:56:21 -0700

    Update reaction_patterns.py

--- detectors/reaction_patterns.py
+++ detectors/reaction_patterns.py
@@ -1,1 +1,13 @@
+import time
+from db import add_signal
 
+REACTION_THRESHOLD_MS = 250  # human reflex lower bound
+
+
+async def check_reaction(reaction, user, reaction_time_ms):
+    if reaction_time_ms < REACTION_THRESHOLD_MS:
+        add_signal(
+            reaction.message.guild.id,
+            user.id,
+            "fast_reaction"
+        )
