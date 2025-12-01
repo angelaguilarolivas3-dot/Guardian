d0c36b7440b6488825e62e62a15e567e8c194ef1
angel <angelaguilarolivas3@gmail.com>
2025-12-01 13:56:42 -0700

    Update join_leave.py

--- detectors/join_leave.py
+++ detectors/join_leave.py
@@ -1,1 +1,7 @@
+from db import add_signal
+import time
 
+def check_join(member):
+    account_age_days = (time.time() - member.created_at.timestamp()) / 86400
+    if account_age_days < 7:
+        add_signal(member.guild.id, member.id, "new_account")
